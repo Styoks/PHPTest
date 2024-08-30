a93630a37fdb3d475e7cd2d76aaabc788bc9f344
Styoks <113370103+Styoks@users.noreply.github.com>
2024-08-30 16:39:44 +0000

    Initial commit

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ function sayHello($name) {
 	echo "Hello $name!";
 }
 
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
 ?>
 
 <html>
@@ -16,11 +23,38 @@ function sayHello($name) {
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
+		<div>
+			<h1>Calculator</h1>
+			<div>
+				<form action="">
+					<input type="text" name="userName" id="userName" placeholder="name">
+					<input type="text" required name="subject1" id="subject1">
+					<input type="text" required name="subject2" id="subject2">
+					<input type="text" name="subject3" id="subject3">
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		</div>
 		<?php 
+		$array = array(
+			($_GET['subject1']),
+			($_GET['subject2']),
+			($_GET['subject3'])
+		);
+
+		$array2 = array(
+			$_GET['userName'] => ($array)
+		);
+
+		if (!is_numeric($_GET['subject1']) || !is_numeric($_GET['subject2']) || (!is_numeric($_GET['subject3']) && $_GET['subject3'] != "")) {
+			debug_to_console(implode("", $array));
+		} else {
+			debug_to_console(array_sum($array));
+		};
+		foreach ($array as $element) {
+		}
 		
-		sayHello('remote world');
-			
-		phpinfo(); 
+		debug_to_console($array2[$_GET['userName']]);
 			
 		?>
 	</body>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5aedbab194e6a09386349c59f7a94218923216a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5aedbab194e6a09386349c59f7a94218923216a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
